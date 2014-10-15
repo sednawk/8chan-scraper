658c8e78ef7cb0636f2da6edf66792bacad42e42
Rude User <sednawk@users.noreply.github.com>
2014-10-15 19:12:58 +0100

    optparse+better dir

--- go.py
+++ go.py
@@ -1,26 +1,48 @@
+#
 # 8Chan Scraper
 # Written by @sednawk and @dade0
 #
 
-import requests
+import os
+import sys
 import urllib
+import requests
 import urlparse
 from bs4 import BeautifulSoup
-import sys
-import os
+from optparse import OptionParser
+
 
 allowed_files = ['webm', 'jpg', 'jpeg', 'gif', 'png']
 files = []
 
-if len(sys.argv) != 3:
-    print "Usage: " + sys.argv[0] + " <url> <output>"
-    sys.exit(1)
+parser = OptionParser()
+parser.add_option("-t", "--thread", dest="thread", help="Thread to scrape.", metavar="THREAD")
+
+parser.add_option("-o", "--out", dest="dir", help="Output directory.", metavar="DIRECTORY")
+parser.add_option("-n", "--new", dest="new", help="Save images into a folder named after the title.", default=None, action='store_true')
+
+(options, args) = parser.parse_args()
+
+
+if not options.thread:
+	parser.print_help()
+	sys.exit()
+
+url = options.thread
+
+if not options.dir:
+	print "[!] No output dir supplied."
+	current = raw_input("Use current dir? [Y/n] ")
+	if current.lower() == "y":
+		dir = os.getcwd() + "/"
+	else:
+		print "bye faget"
+		sys.exit()
 else:
-    url = sys.argv[1]
-    if sys.argv[2].endswith("/"):
-    	dir = sys.argv[2]
-    else:
-    	dir = sys.argv[2] + "/"
+	if options.dir.endswith("/"):
+		dir = options.dir
+	else:
+		dir = options.dir + "/"
 
 resp = requests.get(url).text
 soup = BeautifulSoup(resp)
@@ -31,28 +53,27 @@ if name.endswith(" "):
 	name = name[:-1]
 name = name.replace(" ", "_")
 
-print name
-
-if not os.path.exists(dir + name):
-	os.mkdir(dir + name)
-
 def get_files():
 	for link in soup.findAll('div', {'class': 'file'}):
 		files.append("http://8chan.co" + link.findAll('a')[0]['href'])
 
-def download_files():
+def download_files(new):
 	count = 0
 	for i in files:
 		count += 1
 		print "[+] Image " + str(count)
 		filename = i.split("/")[-1] 
-		#print "download_files: " + filename
-		
-		urllib.urlretrieve(i, dir + "/" + name + "/" +filename)
+		if new:
+			if not os.path.exists(dir + name):
+				os.mkdir(dir + name)
+			urllib.urlretrieve(i, dir + name + "/" +filename)
+		else:
+			print dir +filename
+			urllib.urlretrieve(i, dir +filename)
 
 print "[!] Done!"
 
 
 
 get_files()
-download_files()
+download_files(options.new)
